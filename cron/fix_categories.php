b333700ba9160233e2b7bffb1da2832f07a4c13f
LAPTOP-4AR3KTVN\Asus <kim@epicode.nl>
2021-06-28 22:12:41 +0800

    push all codes for numinix

---
+++ cron/fix_categories.php
@@ -0,0 +1,92 @@
+<?php
+require('includes/configure.php');
+ini_set('include_path', DIR_FS_CATALOG . PATH_SEPARATOR .
+ini_get('include_path'));
+chdir(DIR_FS_CATALOG);
+require_once('includes/application_top.php');
+
+$noProducts = 0;
+$updateResults = 0;
+$catColumn = 'categories_id';
+
+$sQ = selectQuery($noProducts, $db);
+forLoop($sQ, $catColumn);
+updateQuery($sQ, $db);
+
+
+/**
+ * query for viewing categories without products
+ */
+function selectQuery($noProducts, $db) {
+   
+    $sqlSelect = "SELECT * FROM categories WHERE categories_id NOT IN 
+    (SELECT categories.categories_id 
+                    FROM `categories` 
+                    RIGHT OUTER JOIN products_to_categories 
+                    ON categories.categories_id = products_to_categories.categories_id
+                    GROUP BY categories.categories_id) 
+    AND categories_id NOT IN 
+    (SELECT categories.parent_id 
+                    FROM `categories` 
+                    RIGHT OUTER JOIN products_to_categories 
+                    ON categories.categories_id = products_to_categories.categories_id
+                    WHERE categories.parent_id <> 0
+                    GROUP BY categories.parent_id)
+    AND parent_id = 0
+    AND categories_status <> 0
+    GROUP BY categories_id";
+
+    $noProducts = $db->Execute($sqlSelect);
+
+    echo "----------Checking categories without products------------\n";
+    echo 'Number of categories with no products: '. $noProducts->recordCount()."\n";
+
+    return $noProducts;
+}
+
+
+/**
+ * Update / Disable query
+ */
+function updateQuery($results, $db) {
+    if ($results->recordCount() > 0) {
+        echo "\n\n----------Updating/Disable category------------\n";
+
+        $updateSelect = "UPDATE categories SET categories_status = 0
+        WHERE categories_id NOT IN 
+        (SELECT categories.categories_id 
+                        FROM `categories` 
+                        RIGHT OUTER JOIN products_to_categories 
+                        ON categories.categories_id = products_to_categories.categories_id
+                        GROUP BY categories.categories_id) 
+        AND categories_id NOT IN 
+        (SELECT categories.parent_id 
+                        FROM `categories` 
+                        RIGHT OUTER JOIN products_to_categories 
+                        ON categories.categories_id = products_to_categories.categories_id
+                        WHERE categories.parent_id <> 0
+                        GROUP BY categories.parent_id)
+        AND parent_id = 0
+        AND categories_status <> 0";
+    
+        $updateResults = $db->Execute($updateSelect);
+
+        echo 'Number of successfully disabled categories: '.$db->affectedRows();
+    }
+}
+
+
+
+/**
+ * for loop / viewing
+ */
+function forLoop($results, $catColumn) {
+    if ($results->recordCount() > 0) {
+        foreach ($results as $res) {
+            echo "Category ID: ".$res[$catColumn]."\n";
+        }
+    }
+}
+
+
+
